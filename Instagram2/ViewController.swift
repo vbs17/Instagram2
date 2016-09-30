ff7921cc8f2815613ff04128f24b0eeb4aaf3efd
kei ikeuchi <sister@sister.local>
2016-09-30 18:37:49 +0900

    Initial Commit

---
+++ Instagram2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram2
+//
+//  Created by kei ikeuchi on 2016/09/30.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
